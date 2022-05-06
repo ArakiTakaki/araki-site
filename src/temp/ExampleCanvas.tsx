454522576fdbe47bda69e229df6b86d2b2f4f230
ArakiTakaki <arakitakaki.work@gmail.com>
2022-05-06 23:59:13 +0900

    update

--- src/temp/ExampleCanvas.tsx
+++ src/temp/ExampleCanvas.tsx
@@ -4,8 +4,8 @@ import { minMax } from '../utils/math';
 import { reactionDeffusion } from '../utils/reactionDiffusion';
 
 export const ExampleCanvas = () => {
-    const WIDTH = 200;
-    const HEIGTH = 200;
+    const WIDTH = 100;
+    const HEIGTH = 100;
 
     const refCanvas = useRef<HTMLCanvasElement>(null);
     const refContext = useRef<CanvasRenderingContext2D>();
@@ -24,15 +24,21 @@ export const ExampleCanvas = () => {
         reactionDeffusionUpdate();
         reactionDeffusionUpdate();
         reactionDeffusionUpdate();
+        reactionDeffusionUpdate();
         const t = reactionDeffusionUpdate();
         for (let i = 0; i < HEIGTH; i++) {
             for (let j = 0; j < WIDTH; j++) {
                 const a = minMax(t[i][j], 0, 1);
-                const l = Math.floor(a * 100);
+                const l = Math.floor(a * a * 100);
                 refContext.current.fillStyle = 'hsl(0, 0%,' + l + '%)';
                 refContext.current.fillRect(i, j, 10, 10);
             }
         }
     });
-    return <canvas ref={refCanvas} width={WIDTH} height={HEIGTH} />
+
+    return (
+        <div className='h-full w-full flex justify-center'>
+            <canvas ref={refCanvas} width={WIDTH} height={HEIGTH} className="max-w-screen max-h-screen h-full aspect-square" />
+        </div>
+    )
 };
