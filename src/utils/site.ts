74508d1e6e830436d7c68031091161ecdf768547
ArakiTakaki <arakitakaki.work@gmail.com>
2022-05-08 01:49:00 +0900

    deploy

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -3,7 +3,7 @@ import path from 'path-browserify';
 
 export const getUrl = (...url: string[]) => {
     if (process.env.NODE_ENV === 'production') {
-        return path.resolve('/saihate', ...url);
+        return path.resolve('/', ...url);
     }
     return path.resolve('/', ...url);
 };
