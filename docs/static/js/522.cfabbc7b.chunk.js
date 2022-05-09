d73fc3ef1860839f4ad11475b448ee8dac4a8a45
ArakiTakaki <arakitakaki.work@gmail.com>
2022-05-10 07:19:29 +0900

    update

---
+++ docs/static/js/522.cfabbc7b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmysite=self.webpackChunkmysite||[]).push([[522],{9522:function(s,e,i){i.r(e);i(2791);var c=i(184);e.default=function(){return(0,c.jsxs)("div",{children:["about",(0,c.jsx)("div",{className:"hidden sm:block",children:"pc"}),(0,c.jsx)("div",{className:"sm:hidden",children:"sp"})]})}}}]);
+//# sourceMappingURL=522.cfabbc7b.chunk.js.map
