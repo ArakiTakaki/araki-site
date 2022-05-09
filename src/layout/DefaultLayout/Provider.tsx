502cb19ab8fbb6a6556bcace946a53f617e76b22
ArakiTakaki <arakitakaki.work@gmail.com>
2022-05-10 05:06:19 +0900

    fix: 不要なコメントを削除

--- src/layout/DefaultLayout/Provider.tsx
+++ src/layout/DefaultLayout/Provider.tsx
@@ -1,15 +1,12 @@
 export const DefaultProvider = ({
-    // header,
     navigation,
     content,
 }: { 
-    // header: JSX.Element;
     navigation: JSX.Element;
     content: JSX.Element;
 }): JSX.Element => {
     return (
         <>
-            {/* {header} */}
             <div className='flex z-0'>
                 {navigation}
                 {content}
